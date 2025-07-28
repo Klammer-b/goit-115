b9ae74fb6837c550f4f864b7b600dd53021ced14
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2025-07-28 21:19:29 +0300

    add basic express setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-console.log('Hello world');
-console.log('Hello world');
-console.log('Hello world');
-console.log('Hello world');
-console.log('Hello world');
+import express from 'express';
+import crypto from 'node:crypto';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { ENV_VARS } from './constants/envVars.js';
+
+const app = express();
+
+// middleware
+app.use([
+  (req, res, next) => {
+    req.id = crypto.randomUUID();
+    next();
+  },
+  pino(),
+  cors(),
+]);
+
+// controller
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'Hello world!',
+    id: req.id,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found!',
+    status: 404,
+  });
+});
+
+app.use('/', (err, req, res, next) => {
+  res.status(500).json({
+    status: 500,
+    message: 'Oops error happened in application!',
+    error: err.message,
+  });
+});
+
+const PORT = getEnvVar(ENV_VARS.PORT, 3000);
+app.listen(PORT, () => {
+  console.log(`Server is running in ${PORT} port!`);
+});
